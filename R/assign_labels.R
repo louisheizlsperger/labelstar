a31ab173b1b622e9a92dd6f3d4a23d4baab4388f
louisheizlsperger <louis-jonas.heizlsperger@maximilianeum.de>
2024-06-22 11:21:32 +0200

    Retain existing variable labels in 'assign_labels'

--- R/assign_labels.R
+++ R/assign_labels.R
@@ -1,11 +1,13 @@
 #' Assigns Custom Variable Labels to a Data Frame
 #'
 #' This function assigns custom variable labels to a data frame. If a custom label is provided
-#' for a variable, it assigns that label; otherwise, it assigns the variable name as the label.
+#' for a variable, it assigns that label. If a variable already has a label and is not in the
+#' list of custom labels, it retains the old label. Otherwise, it assigns the variable name as the label.
 #'
 #' @param data A data frame to which labels will be applied.
 #' @param variable_labels A named list of custom labels where the names are the variable names
 #' in the data frame, and the values are the labels to be assigned.
+#' @return The data frame with updated labels.
 #' @export
 assign_labels <- function(data, variable_labels) {
 
@@ -15,8 +17,11 @@ assign_labels <- function(data, variable_labels) {
       # Assign the custom label
       attr(data[[var_name]], "label") <- variable_labels[[var_name]]
     } else {
-      # Assign the variable name as the label if no custom label exists
-      attr(data[[var_name]], "label") <- var_name
+      # Retain existing label if it exists and no new custom label is provided
+      if (is.null(attr(data[[var_name]], "label"))) {
+        # Assign the variable name as the label if no existing label or custom label exists
+        attr(data[[var_name]], "label") <- var_name
+      }
     }
   }
 
